c66aa2e1c61f0d7e360f923eaa00ca78d3f6e32b
Junwoo Park 박준우 차장 (주)두산 정보 <leo.park@doosan.com>
2018-10-25 18:49:12 +0900

    조건검색

--- 조건검색Sample/Form1.Designer.cs
+++ 조건검색Sample/Form1.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.requestConditionButton = new System.Windows.Forms.Button();
+            this.conditionComboBox = new System.Windows.Forms.ComboBox();
+            this.conditionSearchButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(726, 12);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(62, 18);
+            this.axKHOpenAPI1.TabIndex = 0;
+            // 
+            // loginButton
+            // 
+            this.loginButton.AutoEllipsis = true;
+            this.loginButton.Location = new System.Drawing.Point(13, 13);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "로그인";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // requestConditionButton
+            // 
+            this.requestConditionButton.Location = new System.Drawing.Point(104, 13);
+            this.requestConditionButton.Name = "requestConditionButton";
+            this.requestConditionButton.Size = new System.Drawing.Size(75, 23);
+            this.requestConditionButton.TabIndex = 2;
+            this.requestConditionButton.Text = "조건식 호출";
+            this.requestConditionButton.UseVisualStyleBackColor = true;
+            // 
+            // conditionComboBox
+            // 
+            this.conditionComboBox.FormattingEnabled = true;
+            this.conditionComboBox.Location = new System.Drawing.Point(13, 63);
+            this.conditionComboBox.Name = "conditionComboBox";
+            this.conditionComboBox.Size = new System.Drawing.Size(121, 21);
+            this.conditionComboBox.TabIndex = 3;
+            // 
+            // conditionSearchButton
+            // 
+            this.conditionSearchButton.Location = new System.Drawing.Point(141, 63);
+            this.conditionSearchButton.Name = "conditionSearchButton";
+            this.conditionSearchButton.Size = new System.Drawing.Size(75, 23);
+            this.conditionSearchButton.TabIndex = 4;
+            this.conditionSearchButton.Text = "조건검색";
+            this.conditionSearchButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.conditionSearchButton);
+            this.Controls.Add(this.conditionComboBox);
+            this.Controls.Add(this.requestConditionButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.axKHOpenAPI1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button requestConditionButton;
+        private System.Windows.Forms.ComboBox conditionComboBox;
+        private System.Windows.Forms.Button conditionSearchButton;
     }
 }
 
--- 조건검색Sample/Form1.cs
+++ 조건검색Sample/Form1.cs
@@ -12,9 +12,91 @@ namespace 조건검색Sample
 {
     public partial class Form1 : Form
     {
+        List<Condition> conditions = new List<Condition>(); //사용자 조건식 리스트
+
         public Form1()
         {
             InitializeComponent();
+            loginButton.Click += Button_Click;
+            requestConditionButton.Click += Button_Click; //조건식 요청 버튼
+            conditionSearchButton.Click += Button_Click; //사용자 조건식 실행 요청 버튼
+            axKHOpenAPI1.OnEventConnect += API_OnEventConnect;
+            axKHOpenAPI1.OnReceiveConditionVer += API_OnReceiveConditionVer;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (sender.Equals(loginButton))
+            {
+                axKHOpenAPI1.CommConnect();
+            }else if(sender.Equals(requestConditionButton))
+            {
+                axKHOpenAPI1.GetConditionLoad(); //사용자 조건식 목록 요청
+            }else if (sender.Equals(conditionSearchButton))
+            {
+                string conditionName = conditionComboBox.Text;
+                if(conditionName.Length > 0)
+                {
+
+                }
+            }
+        }
+
+        private void API_OnEventConnect(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
+        {
+            if (e.nErrCode == 0) //로그인 성공
+            {
+
+            }
+            else //로그인 실패
+            {
+
+            }
+        }
+
+        private void API_OnReceiveConditionVer(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveConditionVerEvent e)
+        {
+            //사용자 조건식 전달
+            string conditionNameList = axKHOpenAPI1.GetConditionNameList();
+            string[] conditionNameArray = conditionNameList.Split(';');
+            foreach(string conditionInfo in conditionNameArray)
+            {
+                if(conditionInfo.Length > 0)
+                {
+                    Console.WriteLine(conditionInfo);
+                    string[] condition = conditionInfo.Split('^');
+                    Console.WriteLine(condition[0].Trim() + " " + condition[1].Trim());
+                    string index = condition[0].Trim();
+                    string name = condition[1].Trim();
+
+                    Condition conditionObject = new Condition(int.Parse(index), name);
+                    conditions.Add(conditionObject);
+
+                    conditionComboBox.Items.Add(name);
+                }
+                
+            }
+
+            for (int i = 0; i<conditionNameArray.Length; i++)
+            {
+
+            }
+            
+            Console.WriteLine("conditionNameList : " + conditionNameList);
+        }
+    }
+
+    public class Condition
+    {
+        int Index { get; set; }
+        string Name { get; set; }
+
+        public Condition(int index, string name)
+        {
+            this.Index = index;
+            this.Name = name;
+
+
         }
     }
 }
