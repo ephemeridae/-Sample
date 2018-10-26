fd4055b9997f8dec100bddcc8c48daf6fcb15778
Junwoo Park 박준우 차장 (주)두산 정보 <leo.park@doosan.com>
2018-10-26 10:54:45 +0900

    end

--- 조건검색Sample/Form1.Designer.cs
+++ 조건검색Sample/Form1.Designer.cs
@@ -34,6 +34,10 @@
             this.requestConditionButton = new System.Windows.Forms.Button();
             this.conditionComboBox = new System.Windows.Forms.ComboBox();
             this.conditionSearchButton = new System.Windows.Forms.Button();
+            this.conditionSearchListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.realConditionListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -82,11 +86,49 @@
             this.conditionSearchButton.Text = "조건검색";
             this.conditionSearchButton.UseVisualStyleBackColor = true;
             // 
+            // conditionSearchListBox
+            // 
+            this.conditionSearchListBox.FormattingEnabled = true;
+            this.conditionSearchListBox.Location = new System.Drawing.Point(12, 152);
+            this.conditionSearchListBox.Name = "conditionSearchListBox";
+            this.conditionSearchListBox.Size = new System.Drawing.Size(367, 277);
+            this.conditionSearchListBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "조건검색";
+            // 
+            // realConditionListBox
+            // 
+            this.realConditionListBox.FormattingEnabled = true;
+            this.realConditionListBox.Location = new System.Drawing.Point(406, 153);
+            this.realConditionListBox.Name = "realConditionListBox";
+            this.realConditionListBox.Size = new System.Drawing.Size(381, 264);
+            this.realConditionListBox.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(406, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "실시간 조건 종목";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.realConditionListBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.conditionSearchListBox);
             this.Controls.Add(this.conditionSearchButton);
             this.Controls.Add(this.conditionComboBox);
             this.Controls.Add(this.requestConditionButton);
@@ -96,6 +138,7 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -106,6 +149,10 @@
         private System.Windows.Forms.Button requestConditionButton;
         private System.Windows.Forms.ComboBox conditionComboBox;
         private System.Windows.Forms.Button conditionSearchButton;
+        private System.Windows.Forms.ListBox conditionSearchListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox realConditionListBox;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- 조건검색Sample/Form1.cs
+++ 조건검색Sample/Form1.cs
@@ -13,6 +13,7 @@ namespace 조건검색Sample
     public partial class Form1 : Form
     {
         List<Condition> conditions = new List<Condition>(); //사용자 조건식 리스트
+        int screenNo = 1000;
 
         public Form1()
         {
@@ -22,6 +23,8 @@ namespace 조건검색Sample
             conditionSearchButton.Click += Button_Click; //사용자 조건식 실행 요청 버튼
             axKHOpenAPI1.OnEventConnect += API_OnEventConnect;
             axKHOpenAPI1.OnReceiveConditionVer += API_OnReceiveConditionVer;
+            axKHOpenAPI1.OnReceiveTrCondition += API_OnReceiveTrCondition;
+            axKHOpenAPI1.OnReceiveRealCondition += API_OnReceiveRealCondition;
         }
 
         private void Button_Click(object sender, EventArgs e)
@@ -29,15 +32,31 @@ namespace 조건검색Sample
             if (sender.Equals(loginButton))
             {
                 axKHOpenAPI1.CommConnect();
-            }else if(sender.Equals(requestConditionButton))
+            }
+            else if(sender.Equals(requestConditionButton))
             {
                 axKHOpenAPI1.GetConditionLoad(); //사용자 조건식 목록 요청
-            }else if (sender.Equals(conditionSearchButton))
+            }
+            else if (sender.Equals(conditionSearchButton))
             {
                 string conditionName = conditionComboBox.Text;
                 if(conditionName.Length > 0)
                 {
+                   Condition condition = conditions.Find(o => o.Name.Equals(conditionName));
+                    if (condition != null)
+                    {
+                        int conditionIndex = condition.Index;
 
+                        int result = axKHOpenAPI1.SendCondition(GetScreenNum(), conditionName, conditionIndex, 1); //실시간 조건 검색(1분동안 같은 조건 검색을 못함)
+                        if (result == 1)
+                        {
+                            Console.WriteLine("요청 성공");
+                        }
+                        else
+                        {
+                            Console.WriteLine("요청 실패");
+                        }
+                    }
                 }
             }
         }
@@ -84,12 +103,67 @@ namespace 조건검색Sample
             
             Console.WriteLine("conditionNameList : " + conditionNameList);
         }
+
+        private string GetScreenNum()
+        {
+            if (screenNo >= 99990)
+            {
+                screenNo = 1000;
+            }
+            screenNo++;
+            return screenNo.ToString();
+        }
+
+        private void API_OnReceiveTrCondition(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrConditionEvent e)
+        {
+            int conditionIndex = e.nIndex;            
+            string codeList = e.strCodeList;
+            string[] codeArray = codeList.Split(';');
+            string conditionName = e.strConditionName;
+
+            conditionSearchListBox.Items.Add("조건식 인덱스 : " + conditionIndex + " 조건식 이름" + conditionName);
+            conditionSearchListBox.Items.Add(codeList);
+
+            foreach(string code in codeArray)
+            {
+                if (code.Length > 0)
+                {
+                    string itemName = axKHOpenAPI1.GetMasterCodeName(code);
+                    conditionSearchListBox.Items.Add(itemName);
+                }
+                
+            }
+                       
+        }
+
+        private void API_OnReceiveRealCondition(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveRealConditionEvent e)
+        {
+            string conditionIndex = e.strConditionIndex;
+            string conditionName = e.strConditionName;
+
+            realConditionListBox.Items.Add("조건식 명 : "+ conditionName + " 조건식 인덱스 : "+ conditionIndex);
+
+            string itemCode = e.sTrCode; //편입 또는 이탈 종목 코드
+            string type = e.strType;     //편입 (I) 이탈 (D)
+            
+            if (type.Equals("I"))
+            {
+                realConditionListBox.Items.Add("종목편입 : " + axKHOpenAPI1.GetMasterCodeName(itemCode) + " " + itemCode);
+                //axKHOpenAPI1.SetRealReg(GetScreenNum(), itemCode, , );
+            }
+            else if(type.Equals("D"))
+            {
+                realConditionListBox.Items.Add("종목이탈 : " + axKHOpenAPI1.GetMasterCodeName(itemCode) + " " + itemCode);
+                //axKHOpenAPI1.SetRealRemove(GetScreenNum(), itemCode, , );
+            }
+
+        }
     }
 
     public class Condition
     {
-        int Index { get; set; }
-        string Name { get; set; }
+        public int Index { get; set; }
+        public string Name { get; set; }
 
         public Condition(int index, string name)
         {
@@ -99,4 +173,6 @@ namespace 조건검색Sample
 
         }
     }
+
+    
 }
